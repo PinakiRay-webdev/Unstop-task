9d98c5ef1c043069276d537710a8d7f03a7c151d
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-12-12 17:32:31 +0530

    implementation of protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,43 @@
-import React from 'react'
-import { createBrowserRouter , RouterProvider } from 'react-router-dom'
-import Login from './Authentication/Login'
-import Home from './Pages/Home/Home'
+import React from "react";
+import { createBrowserRouter, Navigate, Outlet, RouterProvider } from "react-router-dom";
+import Login from "./Authentication/Login";
+import Home from "./Pages/Home/Home";
 const App = () => {
+
+  //developing here a router protection so that no one can access the home page via url params with out login.
+  const getUser = JSON.parse(localStorage.getItem('userCredentials'));
+  const isAuthenticated = getUser ? true : false;
+  
+  const ProtectedRoute = () =>{
+    return isAuthenticated ? <Outlet/> : <Navigate to="/login"/>
+  }
 
   const endpoints = createBrowserRouter([
     {
-      path : '/',
-      element : <><Home/></>
+      path: "/",
+      element : <ProtectedRoute/>,
+      children : [
+        {
+          path : '/',
+          element : <Home/>
+        }
+      ]
     },
     {
-      path : '/login',
-      element : <><Login/></>
-    }
-  ])
+      path: "/login",
+      element: (
+        <>
+          <Login />
+        </>
+      ),
+    },
+  ]);
 
   return (
     <div>
       <RouterProvider router={endpoints}></RouterProvider>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
